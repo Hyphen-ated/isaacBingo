75b253b87290397451d6cd7b3592589fd9deda69
Hyphen-ated <hyphenated@fake>
2014-03-11 01:23:32 -0700

    add goals from brainstorming with dune. remove obsolete explanation text now that we have the pro tip box. add credits with links to people. make it so you can spam clicking on generate board and the link won't move

--- goals.js
+++ goals.js
@@ -16,25 +16,32 @@ easyGoals = [
 "20 Bombs|20bombs",
 "Kill a Green Champion Mob or Boss|greenChamp",
 "Satisfy a Sacrifice Room|satisfySacRoom|Sacrifice rooms are the ones with spikes inside, not teeth in the door. You must get a treasure from it.",
-"Teleport Out of a Boss Fight|teleportBoss",
+"Teleport Out of a Boss Fight|teleportBoss|Forget me now counts.",
 "Any Mushroom|mushroom",
 "Syringe Item|syringe",
 "Fire a Homing Tear|homing|Followers' homing tears count",
+"Fire a Spectral Tear|spectral|Followers' spectral tears count",
 "Bomb a Demon Beggar|bombDemonBeggar",
 "2 Items With \"Mom\" in the Name|momItems",
 "Read a Book|readABook|(not a sports page, not a magazine)",
 "3 different pills|3pills|Must be on a single run",
 "Multishot|multishot|Quad Shot, Triple Shot, Tammy's Head, Loki's Horns, Mom's Eye, Harlequin Baby, Rainbow Baby",
+"Buy Everything in a Shop|clearShop",
+"Horn(s)|horns|Pentagram, active Whore of Babylon, Loki's Horns, Unicorn/Chariot, Brimstone, activate The Nail, Curved Horn",
+"Kill an enemy with a troll bomb|trollKill|Other damage is ok as long as the bomb performs the kill",  
+
 ]
 
 mediumGoals = [
 "Full Clear XL Floor Past Basement|fullClearXL|All normal rooms and the boss",
 "Permanent Flight|permaFlight|Ponies don't count",
 "100 Combined Bombs, Keys, Coins|100combinedThings|Example : 50 coins + 30 bombs + 20 Keys",
+"25+ each of Bombs, Keys, Coins|25eachThings",
 "Map or Compass|permaMap",
 "3 Orbitals|3orbitals|Black fly, meat cube, dagger, guardian angel (not red or blue flies)",
 "Health Drop Follower|healthFollower|Lil chad, the relic",
 "2 Items That Move and Shoot|2shootingFollowers|Any baby follower, meat sphere, etc",
+"2 passive followers|passives|Lil Chad, Relic, 1 up, Dead Cat, Bum Friend, Bomb Bad, Sack of Pennies. Not orbitals.",
 "3 Devil Deals|3devilDeals|Must be on a single run. Finding devil items outside a deal room doesn't count",
 "Clear a Boss Challenge Room|bossChallengeRoom|These are the rooms you have to be at 1hp or less to enter",
 "Enter 3 Super Secret Rooms|3superSecret|Must be on a single run",
@@ -52,11 +59,14 @@ mediumGoals = [
 "Skatole|skatole",
 "Kill a Super or Ultra Sin Miniboss|specialSin",
 "Cut Something|cutSomething|Knife, Dagger, Shears, Blood rights, Razor blade",
-"Kill a Boss Without Shooting or Space Items or Bombs|bossOrbitals|For example, with orbitals",
+"Kill a Boss Without Shooting or Space Items or Bombs|bossOrbitals|For example, with orbitals. Knife counts if you don't press your shoot controls.",
 "Gold Key|goldKey|Dad's Key doesn't count",
 "Beat Heart Without Ever Using a Space Item|killHeartNoSpace",
 "Beat Heart With 1 Heart|heart1heart|Start the heart fight at 1 or less health and don't go above 1 until it dies. Blue and white hearts count as health.",
 "Resurrection|resurrection|Die with 1UP, Ankh, Dead Cat",
+"1 each of red, white, and blue hearts|redwhiteblue|Half a red or blue is okay. Extra empty reds are okay.",
+"Kill a boss with one bomb|onebomb|No damage from any other source. If your first bomb misses, you failed. Missiles and ipecacs aren't bombs. Mr Boom and Dr Fetus are bombs.",
+"3 stars on minimap at once|3stars|In different rooms",
 ]
 
 hardGoals = [
@@ -72,10 +82,15 @@ hardGoals = [
 "Beat Isaac Without Opening any Chests|isaacNoChest|From Basement to Isaac",
 "Beat Isaac Without Using any Bombs on the Run|isaacNoBombs|From Basement to Isaac",
 "Beat Isaac Without Shopping|isaacNoShop|From Basement to Isaac",
-"Damage Isaac With Pee or Holy Water|peeIsaac|Isaac the boss, not Isaac the player character",
+"Pee on or Baptise Isaac|peeIsaac|Isaac the boss, not Isaac the player character. (use #1, lemon mishap, or holy water)",
 "Full Clear Cathedral|fullClearCath|All normal rooms and the boss",
 "No Damage on Womb 2 or XL|noHitWomb2|Health can't go down for any reason",
 "Full Clear and No Damage Caves 1 and 2 or XL|noHitCaves|All normal rooms and the boss. Health can't go down for any reason",
+"Full Clear and No Damage Depths 1 and 2 or XL|noHitDepths|All normal rooms and the boss. Health can't go down for any reason",
+"See 6 Babies During Isaac Fight and Survive|6angelBabies|There have to be 6 of them alive at once and you must then beat Isaac",
+"Enter the Super Secret Room on The Chest|superSecretChest",
+"Clear the Non-Boss Rooms on the Chest|nonBossChestClear|All the rooms except blue baby's",
+"Satisfy 2 Sacrifice Rooms or Demon Beggars and then Beat Heart|sacRoomsBeggarsHeart|You can mix and match the rooms and beggars. Satisfying things you find after the heart doesn't count.",
 ];
 
 
@@ -88,6 +103,8 @@ var exclusions = [
 ["satanNoChest", "isaacNoChest"],
 ["satanNoShop", "isaacNoShop"],
 ["meatSphere", "2shootingFollowers"],
-["100combinedThings", "55coins"],
-["noHitWomb2", "noHitCaves"],
+["100combinedThings", "55coins", "25eachThings"],
+["noHitWomb2", "noHitCaves", "noHitDepths"],
+["blueBaby", "superSecretChest", "nonBossChestClear"],
+
 ];
